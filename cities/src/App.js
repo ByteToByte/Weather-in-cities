68fee8623904744766635528a8ca20de5ba5ebbd
SULEYMAN <vanatiev321@icloud.com>
2019-12-05 19:01:46 +0300

    ApiWeather

--- cities/src/App.js
+++ cities/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Info from "./components/info"
+import Form from "./components/form"
+import Weather from "./components/weather"
+
+const API_KEY = "90b7882bfe55431d2435e59c125818a4"
 
 class App extends React.Component {
+
+  gettingWeather = async () => {
+    const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`);
+    const data = await api_url.json();
+    console.log(data);
+    
+
+  }
+
   render() {
     return (
       <div>
        <Info/>
+       <Form/>
+       <Weather/>
       </div>
     )
   }
