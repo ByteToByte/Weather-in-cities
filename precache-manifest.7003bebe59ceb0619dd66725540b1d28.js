947097b90e66e49871b07dadede589609b2e6168
SULEYMAN <vanatiev321@icloud.com>
2019-12-06 05:53:00 +0300

    Updates

---
+++ precache-manifest.7003bebe59ceb0619dd66725540b1d28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d6bb34b9e181bfb0fcbcfc47cfe9a5",
+    "url": "/Weather-in-cities/index.html"
+  },
+  {
+    "revision": "d1aa5d77a89bca17e3c3",
+    "url": "/Weather-in-cities/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3dca3d8bf6f2ca31def6",
+    "url": "/Weather-in-cities/static/css/main.67fe87b2.chunk.css"
+  },
+  {
+    "revision": "d1aa5d77a89bca17e3c3",
+    "url": "/Weather-in-cities/static/js/2.f57e8e6d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Weather-in-cities/static/js/2.f57e8e6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dca3d8bf6f2ca31def6",
+    "url": "/Weather-in-cities/static/js/main.db16195a.chunk.js"
+  },
+  {
+    "revision": "eb1d2c9fc18f0e6fb80d",
+    "url": "/Weather-in-cities/static/js/runtime-main.957d8533.js"
+  },
+  {
+    "revision": "771ddf391078943cbeefe04da0576876",
+    "url": "/Weather-in-cities/static/media/polygon.771ddf39.jpeg"
+  }
+]);
